7d2b72949addb2d4e7507a5032f05d3cb9dbeb6c
Tayyib Okur <ultratayyib@gmail.com>
2024-07-11 17:48:58 +0300

    splash screen

--- app/src/main/java/com/tayyib/savelocation/MainActivity.kt
+++ app/src/main/java/com/tayyib/savelocation/MainActivity.kt
@@ -3,11 +3,14 @@ package com.tayyib.savelocation
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
